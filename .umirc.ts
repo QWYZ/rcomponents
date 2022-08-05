3be1b6f11e35f52932b6a41af351960d8d2cbbcc
dingtalk_wrbgrt <wx_78471b4ef2e740829c602091f9758063@git.code.tencent.com>
2022-08-05 09:51:19 +0800

    修改文档包资源地址指向

--- .umirc.ts
+++ .umirc.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
-  base: '/qwyz-rcomponent/docs-dist/',
-  publicPath: '/qwyz-rcomponent/docs-dist/',
+  base: '/rcomponent/docs-dist/',
+  publicPath: '/rcomponent/docs-dist/',
   history: {
     type: 'hash', // 设置路由模式为 hash 模式，防止部署至 GitHub Pages 后刷新网页后出现 404 的情况发生.
   },
-  title: 'qwyz-rcomponents',
+  title: 'rcomponents',
   favicon:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
