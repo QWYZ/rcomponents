c03b925aa970fd0ab075e6f9e441f86c97790b9a
dingtalk_wrbgrt <wx_78471b4ef2e740829c602091f9758063@git.code.tencent.com>
2022-08-05 10:36:53 +0800

    1

--- .umirc.ts
+++ .umirc.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
-  // base: '/rcomponent/docs-dist/',
-  // publicPath: '/rcomponent/docs-dist/',
-  // history: {
-  //   type: 'hash', // 设置路由模式为 hash 模式，防止部署至 GitHub Pages 后刷新网页后出现 404 的情况发生.
-  // },
+  base: '/rcomponents/docs-dist/',
+  publicPath: './',
+  history: {
+    type: 'hash', // 设置路由模式为 hash 模式，防止部署至 GitHub Pages 后刷新网页后出现 404 的情况发生.
+  },
   title: 'rcomponents',
   favicon:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
